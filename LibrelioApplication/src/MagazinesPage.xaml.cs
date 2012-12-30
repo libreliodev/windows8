b290261aa376b91f6db4225bdc1f6e73a916e099
voyagerj <voyager_j@mail.ru>
2012-12-30 08:18:57 +0400

    Rearrange the project structure. In additional to Wind magazine project Water magazine project added (second stub magazine). Common source placed in src folder. Common assets in Assets folder. Magazine specific sources/assets/styles placed inside project folder.

---
+++ LibrelioApplication/src/MagazinesPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using LibrelioApplication.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234233
+
+namespace LibrelioApplication
+{
+    /// <summary>
+    /// A page that displays a collection of item previews.  In the Split Application this page
+    /// is used to display and select one of the available groups.
+    /// </summary>
+    public sealed partial class ItemsPage : LibrelioApplication.Common.LayoutAwarePage
+    {
+        public ItemsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            //var dataSrc = new LibrelioApplication.Data.MagazineDataSource();
+            //this.DefaultViewModel["Items"] = dataSrc;
+            //// TODO: Create an appropriate data model for your problem domain to replace the sample data
+            //var sampleDataGroups = MagazineDataSource.GetGroups((String)navigationParameter);
+            var sampleDataGroups = new MagazineDataSource(1);
+            this.DefaultViewModel["Items"] = sampleDataGroups.AllMagazines;
+        }
+
+        /// <summary>
+        /// Invoked when an item is clicked.
+        /// </summary>
+        /// <param name="sender">The GridView (or ListView when the application is snapped)
+        /// displaying the item clicked.</param>
+        /// <param name="e">Event data that describes the item clicked.</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            //// Navigate to the appropriate destination page, configuring the new page
+            //// by passing required information as a navigation parameter
+            //var groupId = ((SampleDataGroup)e.ClickedItem).UniqueId;
+            //this.Frame.Navigate(typeof(SplitPage), groupId);
+        }
+
+        private void itemGridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int i1 = 2;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            int i2 = 2;
+        }
+
+        private T FindChild<T>(UIElement element, Func<T, bool> isObject)
+                     where T : UIElement
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+                var uiElement = VisualTreeHelper.GetChild(element, i) as UIElement;
+                var child = uiElement as T;
+                if (child != null)
+                {
+                    if (isObject(child))
+                        return child;
+                }
+
+                var result = FindChild(uiElement, isObject);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+            return null;
+        }
+
+        private List<T> FindChilds<T>(UIElement element, Func<T, bool> isObject, List<T> res = null)
+                     where T : UIElement
+        {
+            if( res == null )
+                res = new List<T>();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+                var uiElement = VisualTreeHelper.GetChild(element, i) as UIElement;
+                var child = uiElement as T;
+                if (child != null)
+                {
+                    if (isObject(child))
+                        res.Add(child);
+                }
+
+                FindChilds(uiElement, isObject, res);
+            }
+            return res;
+        }
+
+
+    }
+}
---
+++ LibrelioApplication/src/Utils/Utils.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace LibrelioApplication.Utils
+{
+    class Utils
+    {
+        //public static async Task<bool> fileExistAsync(StorageFolder folder, string fileName)
+        public static async Task<bool> fileExistAsync(StorageFolder folder, string fileName)
+        {
+            try
+            {
+                await folder.GetFileAsync(fileName);
+                return true;
+            }
+            catch( Exception e )
+            {
+                return false;
+            }
+        }
+
+
+        async public static void copyFolder(StorageFolder from, StorageFolder to)
+        {
+            await to.CreateFolderAsync(from.Name, CreationCollisionOption.OpenIfExists);
+            IReadOnlyList<StorageFile> storageFiles = await from.GetFilesAsync();
+            foreach (var storageFile in storageFiles)
+            {
+                await storageFile.CopyAsync(to, storageFile.Name, NameCollisionOption.ReplaceExisting);
+            }
+
+            IReadOnlyList<StorageFolder> storageFolders = await from.GetFoldersAsync();
+            foreach (var storageFolder in storageFolders)
+            {
+                copyFolder(storageFolder, to);
+            }
+
+        }
+
+        async public static void prepareTestData()
+        {
+            StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            StorageFolder init = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
+            init = await init.GetFolderAsync("test");
+
+
+            IReadOnlyList<StorageFile> storageFiles = await init.GetFilesAsync();
+            foreach (var storageFile in storageFiles)
+            {
+                await storageFile.CopyAsync(folder, storageFile.Name, NameCollisionOption.ReplaceExisting);
+            }
+
+            IReadOnlyList<StorageFolder> storageFolders = await init.GetFoldersAsync();
+            foreach (var storageFolder in storageFolders)
+            {
+                copyFolder(storageFolder, folder);
+            }
+
+            //copyFolder(init, folder);
+
+            //IReadOnlyList<StorageFile> storageFiles = await init.GetFilesAsync();
+            //foreach (var storageFile in storageFiles)
+            //{
+            //    await storageFile.CopyAsync(folder, storageFile.Name, NameCollisionOption.ReplaceExisting);
+            //}
+        }
+
+
+
+    }
+}
