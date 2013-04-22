960747f2f2671b2a82147967d869ec0e55b574c9
Dorin Damaschin <dorin.damaschin@gmail.com>
2013-04-22 12:55:39 +0300

    added main grid fix

--- LibrelioApplication/LibrelioMagazine/Common/ScrollViewerOffsetMediator.cs
+++ LibrelioApplication/LibrelioMagazine/Common/ScrollViewerOffsetMediator.cs
@@ -13,6 +13,30 @@ using Windows.UI.Xaml.Data;
 
 namespace LibrelioApplication.Common
 {
+    public class VariableGridView : GridView
+    {
+        protected override void PrepareContainerForItemOverride(Windows.UI.Xaml.DependencyObject element, object item)
+        {
+            try
+            {
+                dynamic _Item = item;
+                element.SetValue(Windows.UI.Xaml.Controls.VariableSizedWrapGrid.ColumnSpanProperty, _Item.ColSpan);
+                element.SetValue(Windows.UI.Xaml.Controls.VariableSizedWrapGrid.RowSpanProperty, _Item.RowSpan);
+            }
+            catch
+            {
+                element.SetValue(Windows.UI.Xaml.Controls.VariableSizedWrapGrid.ColumnSpanProperty, 1);
+                element.SetValue(Windows.UI.Xaml.Controls.VariableSizedWrapGrid.RowSpanProperty, 1);
+            }
+            finally
+            {
+                base.PrepareContainerForItemOverride(element, item);
+            }
+        }
+    }
+
+
+
     /// <summary>
     /// Mediator that forwards Offset property changes on to a ScrollViewer
     /// instance to enable the animation of Horizontal/VerticalOffset.
